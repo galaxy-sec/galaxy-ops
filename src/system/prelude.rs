8f330bf5c4019d11c1a45e6638d5de8bc31eb0d4
wukong <sec-wukong@outlook.com>
2025-09-01 16:16:34 +0800

    work

---
+++ src/system/prelude.rs
@@ -0,0 +1,21 @@
+// 导入全局prelude
+pub use crate::predule::*;
+
+// system特定的导入
+pub use crate::error::SysReason;
+pub use crate::system::spec::SysModelSpec;
+pub use crate::types::{LocalizeOptions, SystemLocalizable};
+
+pub use crate::const_vars::{SYS_MODEL_SPC_ROOT, VALUE_DIR};
+
+// system内部常用导入
+pub use orion_infra::auto_exit_log;
+pub use orion_variate::vars::{VarCollection, VarDefinition};
+
+pub use crate::system::{
+    mod_list::ModulesList,
+    path::{SysTargetPaths, SysValuePaths},
+    setting::{ModSetting, SysSetting},
+};
+
+pub use crate::types::{Accessor, InsUpdateable, RefUpdateable};
